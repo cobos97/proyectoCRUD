c01be54703198a740a8c66b2b33b9b52aa05fc70
Antonio Miguel Cobos Fernández <cobosmoriles@gmail.com>
2018-11-20 21:01:39 +0100

    Favicon cambiado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { Routes, RouterModule } from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import {Routes, RouterModule} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { InicioComponent } from './inicio/inicio.component';
-import { HeaderComponent } from './header/header.component';
-import { FormsModule } from '@angular/forms';
-import { AddjugComponent } from './jugadores/addjug/addjug.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import {AppComponent} from './app.component';
+import {InicioComponent} from './inicio/inicio.component';
+import {HeaderComponent} from './header/header.component';
+import {FormsModule} from '@angular/forms';
+import {AddjugComponent} from './jugadores/addjug/addjug.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 const routes: Routes = [
-    { path: '', component: InicioComponent },
-    { path: 'addjug', component: AddjugComponent},
-    { path: '**', component: InicioComponent}
+    {path: '', component: InicioComponent},
+    {path: 'addjug', component: AddjugComponent},
+    {path: '**', component: InicioComponent}
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InicioComponent,
-    HeaderComponent,
-    AddjugComponent
-  ],
-  imports: [
-    BrowserModule,
-      MDBBootstrapModule.forRoot(),
-      RouterModule.forRoot(routes),
-      FormsModule,
-      ReactiveFormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        InicioComponent,
+        HeaderComponent,
+        AddjugComponent
+    ],
+    imports: [
+        BrowserModule,
+        MDBBootstrapModule.forRoot(),
+        RouterModule.forRoot(routes),
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
