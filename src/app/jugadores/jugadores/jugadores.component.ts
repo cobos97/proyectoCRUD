f55135b8b00273eee49c1f80f58a497962a38a4c
Antonio Miguel Cobos Fernández <cobosmoriles@gmail.com>
2018-11-26 18:54:03 +0100

    Por fin lista los jugadores

--- src/app/jugadores/jugadores/jugadores.component.ts
+++ src/app/jugadores/jugadores/jugadores.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AngularFireDatabase, AngularFireList, PathReference, QueryFn} from 'angularfire2/database';
-import {map} from "rxjs/operators";
-import {any} from "codelyzer/util/function";
+import {map} from 'rxjs/operators';
+import {any} from 'codelyzer/util/function';
 
 @Component({
     selector: 'app-jugadores',
@@ -17,24 +17,17 @@ export class JugadoresComponent implements OnInit {
     }
 
     ngOnInit() {
-        console.log("Aquí");
-        this.db.list('/jugadores').snapshotChanges().pipe(map(item=>{
-            return item.map( a => {
+        console.log('Aquí');
+        this.db.list('/jugadores').snapshotChanges().pipe(map(item => {
+            return item.map(a => {
                     const data = a.payload.val();
-                    //cons key=a.payload.key;
+                    // cons key=a.payload.key;
                     return {data};
                 }
             );
         })).subscribe(
-            jugadores =>{
-                console.log(jugadores);
-                //this.jugadores$ = data[0].data;
-
+            jugadores => {
                 this.jugadores$ = jugadores;
-
-
-                console.log(this.jugadores$);
-
             }
         );
 
